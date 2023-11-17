11014c8bcac66752811719cf95781f1eb758f34e
MechacSOHE <112973187+MechacSOHE@users.noreply.github.com>
2023-11-17 02:33:02 +0000

    Create script.py

---
+++ script.py
@@ -0,0 +1,75 @@
+import os
+from io import BytesIO
+from queue import Queue
+import requests
+from flask import Flask, request
+from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryHandler, Dispatcher
+from bs4 import BeautifulSoup
+
+TOKEN = 6739487737:AAE5OAZyWLsvkFVGOCjU6o1zPyxQLkpb8Q4
+URL = os.getenv("URL")
+bot = Bot(TOKEN)
+
+api_key = "3934b457b5c66f9c74c05ee2cf414419267ab8cb"
+url_list = {}
+
+def welcome(update, context) -> None:
+    # ... (votre code actuel)
+
+def search_and_reply(query, chat_id):
+    # ... (votre code actuel)
+
+def get_movie_details(query, chat_id):
+    movie_details = get_movie(query, api_key)
+    img_url = movie_details["img"]
+    caption = f"🎥 {movie_details['title']}\n\n"
+    links = movie_details["links"]
+    for i in links:
+        caption += f"🎬 {i}\n{links[i]}\n\n"
+
+    bot.send_photo(chat_id, photo=img_url, caption=caption)
+
+# ... (le reste de votre code actuel)
+
+def search_movies(query):
+    # ... (votre code actuel)
+
+def get_movie(query, api_key):
+    # ... (votre code actuel)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+@app.route('/{}'.format(TOKEN), methods=['GET', 'POST'])
+def respond():
+    update = Update.de_json(request.get_json(force=True), bot)
+    setup().process_update(update)
+    return 'ok'
+
+@app.route('/setwebhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+@app.route('/search_movie', methods=['POST'])
+def search_movie_route():
+    data = request.get_json()
+    chat_id = data['message']['chat']['id']
+    query = data['message']['text']
+    search_and_reply(query, chat_id)
+    return 'ok'
+
+@app.route('/get_movie_details', methods=['POST'])
+def get_movie_details_route():
+    data = request.get_json()
+    chat_id = data['message']['chat']['id']
+    query = data['message']['text']
+    get_movie_details(query, chat_id)
+    return 'ok'
+  
